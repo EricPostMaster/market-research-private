445cdb5263e05fecc84e59251df16398470d92a7
Eric Sims <58200144+EricPostMaster@users.noreply.github.com>
2021-07-10 22:20:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+mkdir -p ~/.streamlit/
+echo "
+[general]n
+email = "es.sims11@gmail.com"n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]n
+headless = truen
+enableCORS=falsen
+port = $PORTn
 " > ~/.streamlit/config.toml
